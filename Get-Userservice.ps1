ec95638617da8d3a7e682889c8989dfc239df7bf
Morten Fæster <morten.faester@jppol.dk>
2019-09-04 13:06:45 +0200

    Somewhat usefull pipeline.

---
+++ Get-Userservice.ps1
@@ -0,0 +1,12 @@
+. .\Get-AccessToken.ps1
+
+function Get-User([string]$AccessToken, [string]$userid) {
+	$headers = @{ "Authorization" = ("Bearer " + $AccessToken); "Content-Type" = "application/json+jsondate"}
+	$response = Invoke-Webrequest -Uri ("https://userservice.jppol.dk/ssouser.svc/user/" + $userid) -Method GET -Headers $headers
+	$response|ConvertFrom-Json
+}
+
+$accessToken = Get-AccessToken -ClientID $credentials.clientId -ClientSecret $credentials.clientSecret
+foreach($userid in $input) {
+	Get-User -AccessToken $accessToken -userid $userid 
+}
